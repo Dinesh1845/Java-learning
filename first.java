b79bf2f241a163f950fb071e47d7bc9d331a748b
DineshShankar <dineshshankar156@gmail.com>
2024-08-04 17:22:09 +0530

    c2

--- first.java
+++ first.java
@@ -1,8 +1,10 @@
-class nome{
-    public static void main(String a[])
-{
-    
-System.out.print("hello world");
+class first {
+    public static void main(String[] args) {
+        
+        int num1 = 10;
+        int num2 = 20;
+
+        System.out.println(num1+num2);
+    }
 }
 
-}
