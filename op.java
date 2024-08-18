761fa36306780817c86eefaabc414c417a3e5fcf
DineshShankar <dineshshankar156@gmail.com>
2024-08-18 11:27:49 +0530

    testcase

--- op.java
+++ op.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class op {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println( "============================");
         for( int i = 0 ; i<3 ; i++ ){
-            System.out.println( "============================");
+           
             String name = input.nextLine();
+            input.nextLine();
             int n = input.nextInt();
             input.nextInt();
             System.out.printf("-%5s%03d%n" , name , n);
