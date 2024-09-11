2c9870e2324b92e78571fb44ae7f2b5f3561ebc5
DineshShankar <dineshshankar156@gmail.com>
2024-09-11 20:03:20 +0530

    s2

--- i.java
+++ i.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
 
+class i{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int h = input.nextInt();
+        for(int i =0 ; i>h; i++){
+            long x = input.nextLong();
+            if(x <100 && x>200){
+                System.out.println("you're correct your value is beetween 100 to 200");
+            }
+            byte b = input.nextByte();
+            if(b > -128 && b<127){
+                System.out.println("your vanle is beetween 127");
+            }
+        }
+    }
+}
---
+++ w.java
@@ -0,0 +1,7 @@
+public class w {
+    public static void main(String[] args) {
+        int i = 9;
+        int j = 8/i;
+        System.out.println(j);
+    }
+}
